04c8274e7c98a30afae407a6fc7cd920e9cb8826
tobi <tobi19.py@gmail.com>
2024-12-20 21:10:49 -0600

    Implement groups

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -15,9 +21,13 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        player.update(dt)
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
