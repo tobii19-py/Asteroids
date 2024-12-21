c65be2dfe88e7e9e5f593507895cfe874a74b559
tobi <tobi19.py@gmail.com>
2024-12-20 19:30:39 -0600

    Implement player draw method

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from constants import *
 import pygame
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -12,7 +14,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
